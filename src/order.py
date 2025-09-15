97879bcbf0bbd9342c75fc5b33e0cc1f7c96b352
Igor Fernandes <igrfclima@gmail.com>
2025-09-15 17:44:33 -0300

    feat: add constructors and propertys

--- src/order.py
+++ src/order.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Dict
 from menu_item import MenuItem
 
@@ -5,9 +6,29 @@ from menu_item import MenuItem
 class Order:
     """Representa um único pedido feito por um cliente."""
 
+    @property
+    def order_number(self) -> int:
+        return self.__order_number
+    @property
+    def items(self) -> list[tuple[MenuItem, int]]:
+        return self.__items
+    @property
+    def timestamp(self) -> datetime:
+        return self.__timestamp
+    @property
+    def total_price(self) -> float:
+        return self.__total_price
+    @property
+    def status(self) -> str:
+        return self.__status
+
     def __init__(self, order_number: int) -> None:
         """Construtor da classe Order."""
-        pass
+        self.__order_number = order_number
+        self.__items = []
+        self.__timestamp = datetime.now()
+        self.__total_price = 0.0
+        self.__status = "Em Preparação"
 
     def add_item(self, item: MenuItem) -> None:
         """Adiciona um MenuItem ao pedido e recalcula o preço total."""
